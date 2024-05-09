f9244fa3195e3df1af3b90f76e50dcb96807fb49
Doni <donizetti.souza9@gmail.com>
2024-05-09 19:20:45 -0300

    Initial Commit

---
+++ Recipe List/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Recipe List
+//
+//  Created by Donizetti de Souza on 5/9/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Recipe List/Recipe_ListApp.swift
@@ -0,0 +1,17 @@
+//
+//  Recipe_ListApp.swift
+//  Recipe List
+//
+//  Created by Donizetti de Souza on 5/9/24.
+//
+
+import SwiftUI
+
+@main
+struct Recipe_ListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
