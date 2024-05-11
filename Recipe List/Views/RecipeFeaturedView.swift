42485bae260035440712226913c450a75c13a50c
Doni <donizetti.souza9@gmail.com>
2024-05-11 13:53:27 -0300

    - add RecipeFeaturedView, modified dataflow using enviroment modifier, created recipe model instace in the recipetaview

---
+++ Recipe List/Views/RecipeFeaturedView.swift
@@ -0,0 +1,18 @@
+//
+//  RecipeFeaturedView.swift
+//  Recipe List
+//
+//  Created by Donizetti de Souza on 5/11/24.
+//
+
+import SwiftUI
+
+struct RecipeFeaturedView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RecipeFeaturedView()
+}
