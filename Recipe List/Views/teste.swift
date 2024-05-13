7ca1a277c27b7e36868f3f1facc0abbb77e0ff5e
Doni <donizetti.souza9@gmail.com>
2024-05-13 12:02:18 -0300

    - change swiftui list to scrollview lazy vstack and foreach - hidle navigationbar title and use text element for the title in RecipeListView

---
+++ Recipe List/Views/teste.swift
@@ -0,0 +1,18 @@
+//
+//  teste.swift
+//  Recipe List
+//
+//  Created by Donizetti de Souza on 5/13/24.
+//
+
+import SwiftUI
+
+struct teste: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    teste()
+}
