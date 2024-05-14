fa3646074a99d5a0d208bc26bd5eb59a259fb08a
Doni <donizetti.souza9@gmail.com>
2024-05-14 14:42:46 -0300

    -database update

---
+++ Recipe List/Views/RecipeHighlights .swift
@@ -0,0 +1,18 @@
+//
+//  RecipeHighlights .swift
+//  Recipe List
+//
+//  Created by Donizetti de Souza on 5/14/24.
+//
+
+import SwiftUI
+
+struct RecipeHighlights_: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RecipeHighlights_()
+}
